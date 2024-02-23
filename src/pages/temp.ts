4e947ee6bd44aa4aa318c94c4c40b6a49384cad8
tom55dev <thomasbill55dev@gmail.com>
2024-02-23 18:07:15 +0000

    fix?: remove temp.ts

--- src/pages/temp.ts
+++
@@ -1,7 +0,0 @@
-import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
-
-export function temp(wallet : any) {
-    console.log("provider wallet ---->", wallet)
-    const nodewallet = wallet as NodeWallet;
-    console.log("node wallet---->", nodewallet);
-}
