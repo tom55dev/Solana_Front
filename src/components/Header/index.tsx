15c4593ba8a6894ea7301c5626756092c5a12b02
tom55dev <thomasbill55dev@gmail.com>
2024-03-07 04:13:34 +0000

    fix header css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,7 @@ import {
   WalletModalProvider,
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
+import Link from "next/link";
 
 export default function Header() {
   const [isTop, setIsTop] = useState(false);
@@ -28,8 +29,17 @@ export default function Header() {
   }, []);
   return (
     <div
-      className={`w-full flex justify-start items-center px-12 py-8 z-50 header-style gap-4 text-white`}
+      className={`w-full flex justify-between items-center px-12 py-8 z-50 header-style gap-4 text-white fixed`}
     >
+      <div className="flex gap-4 text-white pl-4">
+        <Link href="/">Home</Link>
+        <a
+          href="https://portalbridge.com/advanced-tools/#/transfer"
+          target="_blank"
+        >
+          Bridge
+        </a>
+      </div>
       <div className="rounded-lg h-12 w-[200px] flex justify-center items-center bg-main_r">
         <WalletModalProvider>
           <WalletMultiButton className="text-white" />
